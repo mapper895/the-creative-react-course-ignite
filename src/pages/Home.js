8dda57f86c1c0773dddc6897d04d9d4aba221044
mapper895 <mapper.895.mp@gmail.com>
2021-01-03 13:03:07 -0600

    13. React Router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+// Game detail
+import GameDetail from "../components/GameDetail";
 // Redux
 import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
@@ -7,8 +9,12 @@ import Game from "../components/Game";
 // Styling and animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  // Get the current location
+  const location = useLocation();
+  const pathId = location.pathname.split("/")[2];
   // Fetch Games
   const dispatch = useDispatch();
   useEffect(() => {
@@ -19,6 +25,7 @@ const Home = () => {
 
   return (
     <GameList>
+      {pathId && <GameDetail />}
       <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map((game) => (
