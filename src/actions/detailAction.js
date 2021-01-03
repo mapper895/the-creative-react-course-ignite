029182056045f8047e518b845f41452957f4ae5a
mapper895 <mapper.895.mp@gmail.com>
2021-01-03 13:10:13 -0600

    14. Awesome Redux Trick

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -2,6 +2,10 @@ import axios from "axios";
 import { gameDetailsURL, gameSecreenshotURL } from "../api";
 
 export const loadDetail = (id) => async (dispatch) => {
+  dispatch({
+    type: "LOADING_DETAIL",
+  });
+
   const detailData = await axios.get(gameDetailsURL(id));
   const screenshotData = await axios.get(gameSecreenshotURL(id));
 
