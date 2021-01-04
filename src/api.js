9564af60c391a07781e4df9de645cfa268b181fe
mapper895 <mapper.895.mp@gmail.com>
2021-01-03 19:41:31 -0600

    21.Nav Searching

--- src/api.js
+++ src/api.js
@@ -42,3 +42,6 @@ export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
 // Game screenshots
 export const gameSecreenshotURL = (game_id) =>
   `${base_url}games/${game_id}/screenshots`;
+// Searched game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?search=${game_name}&page_size=9`;
